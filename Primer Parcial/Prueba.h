1db4db9280e0a2c1321e1adda6c5725a0f5443e6
ajcayo <ajcayo@espe.edu.ec>
2026-04-12 22:48:03 -0500

    Add files via upload

---
+++ Primer Parcial/Prueba.h
@@ -0,0 +1,48 @@
+/***********************************************************************
+ * Module:  main.cpp
+ * Author:  Janine
+ * Modified: jueves, 9 de abril de 2026 07:09:19 p. m.
+ * Purpose: Declaration of the class Quebrado
+ ***********************************************************************/
+#include <iostream>
+#include "Quebrado.h"
+#include "Operacion.h"
+
+using namespace std;
+
+int main() {
+    float n, d;
+    Quebrado q1, q2, resultado; 
+
+    cout << "--- Ingrese el primer quebrado ---" << endl;
+    cout << "Numerador: ";
+    cin >> n;
+    q1.setNumerador(n);
+
+    cout << "Denominador: "; 
+    cin >> d;
+    q1.setDenominador(d);
+
+    cout << "\n--- Ingrese el segundo quebrado ---" << endl;
+    cout << "Numerador: ";
+    cin >> n;
+    q2.setNumerador(n);
+
+    cout << "Denominador: ";
+    cin >> d;
+    q2.setDenominador(d);
+
+    Operacion operacion;  
+    resultado = operacion.sumar(q1, q2);
+
+    cout << "\n--- Resultado de la Suma ---" << endl;
+    operacion.mostrar(q1);
+    cout << " + ";
+    operacion.mostrar(q2);
+    cout << " = ";
+    operacion.mostrar(resultado);
+
+    cout << endl;
+
+    return 0;
+}
